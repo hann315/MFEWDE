455e04f1cd54e1c2d7772a90ba5dc2cddb172f29
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-09-18 16:52:15 +0700

    fix on size on touchable, add sweetalert2

--- src/scripts/utils/postReviewInitiator.js
+++ src/scripts/utils/postReviewInitiator.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import RestaurantSource from '../data/restaurant-source';
 import UrlParser from '../routes/url-parser';
 
@@ -13,32 +14,45 @@ const PostReview = async () => {
     review: inputReview.value,
   };
 
-  const date = new Date().toLocaleDateString('id-ID', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString('id-ID', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
 
-  const newReview = `
+  const createReviewItem = (name, date, review) => `
     <div class="detail-review-item">
       <div class="header-review">
-        <p class="name-review">${dataInput.name}</p>
-
+        <p class="name-review">${name}</p>
         <p class="date-review">${date}</p>
       </div>
-
-      <div class="body-review">
-        ${dataInput.review}
-      </div>
+      <div class="body-review">${review}</div>
     </div>
   `;
+
   if (dataInput.name !== '' && dataInput.review !== '') {
     await RestaurantSource.postReview(dataInput);
+    const dateFormatted = formatDate(new Date());
+    // eslint-disable-next-line max-len
+    const newReview = createReviewItem(dataInput.name, dateFormatted, dataInput.review);
     reviewContainer.innerHTML += newReview;
     inputReviewName.value = '';
     inputReview.value = '';
+
+    // Show success message
+    Swal.fire({
+      icon: 'success',
+      title: 'Success!',
+      text: 'Your review has been sent successfully.',
+    });
   } else {
-    alert('Please fill the form!');
+    Swal.fire({
+      icon: 'warning',
+      title: 'Oops...',
+      text: 'Please complete all fields.',
+    });
   }
 };
 
