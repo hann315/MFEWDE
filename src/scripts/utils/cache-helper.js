dd432588c43f32a813c0c0845131cd9cdff19fbb
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-09-24 00:13:20 +0700

    code refactor in Cache_Helper

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -2,7 +2,7 @@ import CONFIG from '../globals/config';
 
 const CacheHelper = {
   async cachingAppShell(requests) {
-    const cache = await this._openCache();
+    const cache = await caches.open(CONFIG.CACHE_NAME);
     await cache.addAll(requests);
   },
 
@@ -17,41 +17,23 @@ const CacheHelper = {
 
   async revalidateCache(request) {
     try {
-      const response = await caches.match(request);
+      const response = await caches.match(request) || await fetch(request);
 
-      if (response) {
-        return response;
+      if (!response || response.status !== 200) {
+        throw new Error('Invalid response');
       }
 
-      return this._fetchRequest(request);
+      await this._addCache(request, response.clone());
+      return response;
     } catch (error) {
       throw new Error(`Error revalidating cache: ${error.message}`);
     }
   },
 
-  async _openCache() {
-    return caches.open(CONFIG.CACHE_NAME);
-  },
-
-  async _fetchRequest(request) {
+  async _addCache(request, response) {
     try {
-      const response = await fetch(request);
-
-      if (!response || response.status !== 200) {
-        return response;
-      }
-
-      await this._addCache(request);
-      return response;
-    } catch (error) {
-      throw new Error(`Error fetching request: ${error.message}`);
-    }
-  },
-
-  async _addCache(request) {
-    try {
-      const cache = await this._openCache();
-      await cache.add(request);
+      const cache = await caches.open(CONFIG.CACHE_NAME);
+      await cache.put(request, response);
     } catch (error) {
       throw new Error(`Error adding to cache: ${error.message}`);
     }
